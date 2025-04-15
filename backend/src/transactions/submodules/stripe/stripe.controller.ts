13403e2f37516fe2503a67ba13088b735181a47a
Gabriel Morris <gabrielm@stork.inc>
2025-04-15 14:31:32 +0100

    further debugging

--- backend/src/transactions/submodules/stripe/stripe.controller.ts
+++ backend/src/transactions/submodules/stripe/stripe.controller.ts
@@ -3,11 +3,21 @@ import { StripeService } from './stripe.service';
 
 @Controller('stripe')
 export class StripeController {
-  constructor(private readonly service: StripeService) {}
+  constructor(private readonly service: StripeService) { }
 
   @Post("/webhook")
-  webhook(@Body() body: any): Promise<void> {
-    // I think that this should be secured as well, to prevent abuse of the system in some way - it can be done later.
-    return this.service.handleEvent(body);
+  async webhook(@Body() body: any): Promise<string> {
+    try {
+      console.log("STRIPE BEING HIT")
+      console.log("STRIPE BODY", body)
+      // I think that this should be secured as well, to prevent abuse of the system in some way - it can be done later.
+      await this.service.handleEvent(body);
+      return "response"
+
+    } catch (e) {
+      console.log("ERROR")
+      console.error("ERROR", e)
+      return "error"
+    }
   }
 }
