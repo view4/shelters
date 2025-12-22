33e363153f6d9c4f9b9b53ae9807863cf8acc164
view4 <morrisgaby987@gmail.com>
2025-12-22 18:10:44 +0000

    handled

--- client/src/modules/releases/components/screens/FlowingMatterReleaseScreen/component.jsx
+++ client/src/modules/releases/components/screens/FlowingMatterReleaseScreen/component.jsx
@@ -1,221 +1,81 @@
 import cx from "classnames"
 import { Button, Card, Container, Feature, Screen, Text, Title, Modal, Drawer, DrawerDialogue } from "modules/Core/sub-modules/ui-kit/exports"
 import ShelterImage from "modules/shelter/assets/shelter.png";
+import JourneyImage from "modules/releases/assets/journey.png"
 import SpeciesImage from "modules/releases/assets/four-species.png"
 import SukkahDwellingsImage from "modules/releases/assets/sukkah-dwelling.png"
-import Etrog from "modules/releases/assets/etrog.png"
-import Lulav from "modules/releases/assets/lulav.png"
-import Myrtle from "modules/releases/assets/myrtle.png"
-import Willow from "modules/releases/assets/willow.png"
+import Etrog from "modules/releases/assets/checklist.png"
+import Lulav from "modules/releases/assets/roadmap.png"
+import Myrtle from "modules/releases/assets/timer.png"
+import Willow from "modules/releases/assets/journal.png"
 import { useMemo, useState } from "react";
 import styles from "./styles.module.scss"
 import { AuthenticationButton } from "modules/auth/components";
 import useAuth from "modules/auth/hooks/useAuth";
 import { AUTH_MODES } from "modules/auth/components/AuthenticationButton";
 import staticJSON from "../../../copy/sukkot-release.json"
+import { Link } from "modules/Core/sub-modules/ui-kit/components/Link";
 const { upcomingFeatures, prospectiveFeatures } = staticJSON;
-/*c
-
-        background-image: url('data:image/svg+xml;utf8,\
-  <svg xmlns="http://www.w3.org/2000/svg" width="240" height="56" viewBox="0 0 240 56">\
-    <rect width="240" height="56" fill="none"/>\
-    <g stroke="black" stroke-width="3" fill="none" stroke-linecap="round">\
-      <!-- top row of thatch touching y=0 (flush) -->\
-      <path d="M0 0 L20 28 L0 56"/>\
-      <path d="M20 0 L40 28 L20 56"/>\
-      <path d="M40 0 L60 28 L40 56"/>\
-      <path d="M60 0 L80 28 L60 56"/>\
-      <path d="M80 0 L100 28 L80 56"/>\
-      <path d="M100 0 L120 28 L100 56"/>\
-      <path d="M120 0 L140 28 L120 56"/>\
-      <path d="M140 0 L160 28 L140 56"/>\
-      <path d="M160 0 L180 28 L160 56"/>\
-      <path d="M180 0 L200 28 L180 56"/>\
-      <path d="M200 0 L220 28 L200 56"/>\
-      <path d="M220 0 L240 28 L220 56"/>\
-      <!-- second layer for texture -->\
-      <path d="M10 0 L30 28 L10 56" opacity=' 0.55'/>\
-      <path d="M30 0 L50 28 L30 56" opacity=' 0.55'/>\
-      <path d="M50 0 L70 28 L50 56" opacity=' 0.55'/>\
-      <path d="M70 0 L90 28 L70 56" opacity=' 0.55'/>\
-      <path d="M90 0 L110 28 L90 56" opacity=' 0.55'/>\
-      <path d="M110 0 L130 28 L110 56" opacity=' 0.55'/>\
-      <path d="M130 0 L150 28 L130 56" opacity=' 0.55'/>\
-      <path d="M150 0 L170 28 L150 56" opacity=' 0.55'/>\
-      <path d="M170 0 L190 28 L170 56" opacity=' 0.55'/>\
-      <path d="M190 0 L210 28 L190 56" opacity="0.55"/>\
-      <path d="M210 0 L230 28 L210 56" opacity="0.55"/>\
-    </g></svg>');
-    */
-const SkachSVG = () => {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="240" height="56" viewBox="0 0 240 56">\
-            <rect width="240" height="56" fill="none" />\
-            <g stroke="black" stroke-width="3" fill="none" stroke-linecap="round">\
-                {/* <!-- top row of thatch touching y=0 (flush) -->\ */}
-                <path d="M0 0 L20 28 L0 56" />\
-                <path d="M20 0 L40 28 L20 56" />\
-                <path d="M40 0 L60 28 L40 56" />\
-                <path d="M60 0 L80 28 L60 56" />\
-                <path d="M80 0 L100 28 L80 56" />\
-                <path d="M100 0 L120 28 L100 56" />\
-                <path d="M120 0 L140 28 L120 56" />\
-                <path d="M140 0 L160 28 L140 56" />\
-                <path d="M160 0 L180 28 L160 56" />\
-                <path d="M180 0 L200 28 L180 56" />\
-                <path d="M200 0 L220 28 L200 56" />\
-                <path d="M220 0 L240 28 L220 56" />\
-                {/* <!-- second layer for texture -->\ */}
-                <path d="M10 0 L30 28 L10 56" opacity=' 0.55' />\
-                <path d="M30 0 L50 28 L30 56" opacity=' 0.55' />\
-                <path d="M50 0 L70 28 L50 56" opacity=' 0.55' />\
-                <path d="M70 0 L90 28 L70 56" opacity=' 0.55' />\
-                <path d="M90 0 L110 28 L90 56" opacity=' 0.55' />\
-                <path d="M110 0 L130 28 L110 56" opacity=' 0.55' />\
-                <path d="M130 0 L150 28 L130 56" opacity=' 0.55' />\
-                <path d="M150 0 L170 28 L150 56" opacity=' 0.55' />\
-                <path d="M170 0 L190 28 L170 56" opacity=' 0.55' />\
-                <path d="M190 0 L210 28 L190 56" opacity="0.55" />\
-                <path d="M210 0 L230 28 L210 56" opacity="0.55" />\
-            </g></svg>
-    )
-}
 
 
-const tabs = [
-    {
-        title: "Features in progress",
-        Component: () => <Container className={styles.upcomingFeaturesTextContainer} p1>
-            {/* <Title>In Progress</Title> */}
-            <Container mt1 />
-            <Text>
-                The following features are currently in development, either already started or comissioned.
-            </Text>
-            <Container mt1 />
+// const tabs = [
+//     {
+//         title: "Features in progress",
+//         Component: () => <Container className={styles.upcomingFeaturesTextContainer} p1>
+//             {/* <Title>In Progress</Title> */}
+//             <Container mt1 />
+//             <Text>
+//                 The following features are currently in development, either already started or comissioned.
+//             </Text>
+//             <Container mt1 />
 
-            <Container>
-                {upcomingFeatures.map((feature) => (
-                    <Feature className={styles.upcomingFeature} key={feature.name} name={feature.name} card content={feature.description} />
-                ))}
-            </Container>
-        </Container>
-    },
-    {
-        title: "Prospective Features",
-        Component: () => <Container p1>
-            {/* <Title>Prospective Features</Title> */}
-            <Container mt1 />
+//             <Container>
+//                 {upcomingFeatures.map((feature) => (
+//                     <Feature className={styles.upcomingFeature} key={feature.name} name={feature.name} card content={feature.description} />
+//                 ))}
+//             </Container>
+//         </Container>
+//     },
+//     {
+//         title: "Prospective Features",
+//         Component: () => <Container p1>
+//             {/* <Title>Prospective Features</Title> */}
+//             <Container mt1 />
 
-            <Text>These features are potential future features that we may consider adding in the future.</Text>
-            <Container mt1 />
+//             <Text>These features are potential future features that we may consider adding in the future.</Text>
+//             <Container mt1 />
 
-            <Container>
-                {prospectiveFeatures.map((feature) => (
-                    <Feature className={styles.prospectiveFeature} key={feature.name} name={feature.name} content={feature.description} card />
-                ))}
-            </Container>
-        </Container>
-    }
-]
+//             <Container>
+//                 {prospectiveFeatures.map((feature) => (
+//                     <Feature className={styles.prospectiveFeature} key={feature.name} name={feature.name} content={feature.description} card />
+//                 ))}
+//             </Container>
+//         </Container>
+//     }
+// ]
 const UpcomingModal = ({ isOpen, onClose }) => {
 
     return (
         <Modal isOpen={isOpen} onClose={onClose} borderless>
             <Card
                 className={styles.upcomingFeaturesCard}
-                // contentClassName={styles.upcoingFeaturesContent} 
-                tabs={tabs}
-                tabProps={{ activeTabIndex: 0 }}
+                // tabProps={{ activeTabIndex: 0 }}
                 headerProps={{
                     className: styles.upcomingFeaturesHeader
                 }}
+                header="What's new?!"
                 borderless
-            />
+            >
+                <Container flex >
+                    You can catch the previous release <Link to="/releases/sukkot"> here.</Link>
+                </Container>
+                <Container mt1 />
+                <Text>Since the previous release - functional subscriptions have been added, and (behind the scenes) there's a new admin dashboard to keep track of the of internal usage now some users have started utilising and exploring the space. </Text>
+
+            </Card>
         </Modal>
     )
 }
-const emojis = [
-    {
-        emoji: "🍋",
-        top: "18%",
-        left: "18%",
-        animationDuration: "9s"
-    },
-    {
-        emoji: "🌾",
-        top: "72%",
-        left: "72%",
-        animationDuration: "10s"
-    },
-    {
-        emoji: "🛖",
-        top: "36%",
-        left: "72%",
-        animationDuration: "10s"
-    },
-    {
-        top: "90%",
-        left: "36%",
-        animationDuration: "10s"
-    },
-    {
-        emoji: "🌿",
-        top: "54%",
-        left: "54%",
-        animationDuration: "10s"
-    },
-    {
-        emoji: "⛺",
-        top: "72%",
-        left: "36%",
-        animationDuration: "10s"
-    },
-    {
-        emoji: "🍂",
-        top: "54%",
-        left: "18%",
-        animationDuration: "10s"
-    },
-    // {
-    //     emoji: "༄",
-    //     top: "90%",
-    //     left: "90%",
-    //     animationDuration: "10s"
-    // },
-    {
-        emoji: "🍷",
-        top: "18%",
-        left: "54%",
-        animationDuration: "10s"
-    },
-    {
-        emoji: "🎋",
-        top: "18%",
-        left: "90%",
-        animationDuration: "10s"
-    },
-    {
-        emoji: "🏜️",
-        top: "36%",
-        left: "36%",
-        animationDuration: "10s"
-    },
-    {
-        emoji: "🛤️",
-        top: "54%",
-        left: "90%",
-        animationDuration: "10s"
-    },
-    {
-        // emoji: "🎉",
-        top: "90%",
-        left: "18%",
-        emoji: "🌴",
-
-        animationDuration: "10s"
-    }
-
-]
 
 const TitleWithSubtext = ({ title, subtext, className }) => {
     return (
@@ -230,7 +90,7 @@ const TitleWithSubtext = ({ title, subtext, className }) => {
     )
 }
 
-const ReleaseScreen = ({ title, releaseDate, features, links, narrative, hasError, releaseKey, intentionalText }) => {
+const ReleaseScreen = ({ title, releaseDate, features, links, narrative, hasError, releaseKey, intentionalText, featuresSubtext, releaseTitle, releaseSubtitle }) => {
     const [isUpcomingDrawerOpen, setIsUpcomingDrawerOpen] = useState(false);
     const [isSubscribitionModalOpen, setIsSubscribitionModalOpen] = useState(false);
     const [activeFeature, setActiveFeature] = useState(null);
@@ -239,10 +99,10 @@ const ReleaseScreen = ({ title, releaseDate, features, links, narrative, hasErro
 
     // Fixed decorative placements near corners
     const decorations = [
-        { src: Etrog, alt: "Etrog", style: { top: "18%", left: "18%", width: "7rem", transform: "rotate(-8deg)", animationDuration: "9s", opacity: 0.25 } },
-        { src: Lulav, alt: "Lulav", style: { top: "72%", right: "72%", width: "8rem", transform: "rotate(6deg)", animationDuration: "10s" } },
-        { src: Myrtle, alt: "Myrtle", style: { bottom: "72%", left: "72%", width: "7rem", transform: "rotate(12deg)", animationDuration: "8s" } },
-        { src: Willow, alt: "Willow", style: { bottom: "18%", right: "18%", width: "9rem", transform: "rotate(-10deg)", animationDuration: "11s" } },
+        { src: Willow, alt: "Etrog", style: { top: "25%", left: "18%", width: "7rem", transform: "rotate(-8deg)", animationDuration: "9s", opacity: 0.25 } },
+        { src: Myrtle, alt: "Lulav", style: { top: "72%", right: "72%", width: "8rem", transform: "rotate(6deg)", animationDuration: "10s" } },
+        { src: Lulav, alt: "Myrtle", style: { top: "18%", left: "72%", width: "7rem", transform: "rotate(12deg)", animationDuration: "8s" } },
+        { src: Etrog, alt: "Willow", style: { top: "72%", right: "18%", width: "9rem", transform: "rotate(-10deg)", animationDuration: "11s" } },
     ];
 
     // Error state modal
@@ -272,7 +132,7 @@ const ReleaseScreen = ({ title, releaseDate, features, links, narrative, hasErro
                             <Text className={styles.date}>{releaseDate}</Text>
                         </Container>
                         <Container className={styles.kadeshTextContainer}>
-                            <Text italic className={styles.kadeshText}> For a seven day period you shall live in booths...<span>(Leviticus 23:40)</span> </Text>
+                            <Text italic className={styles.kadeshText}> {releaseTitle} </Text>
 
                             {/* <Text className={styles.subText}></Text> */}
                         </Container>
@@ -282,7 +142,7 @@ const ReleaseScreen = ({ title, releaseDate, features, links, narrative, hasErro
                     </Container>
                     <Container className={styles.sectionColumn}>
                         <Container className={styles.imageContainer}>
-                            <img src={ShelterImage} alt="Shelter" />
+                            <img src={JourneyImage} alt="Shelter" />
                         </Container>
                     </Container>
                 </Card>
@@ -296,7 +156,7 @@ const ReleaseScreen = ({ title, releaseDate, features, links, narrative, hasErro
                     </Container>
 
                     <Container className={styles.featuresContent}>
-                        <TitleWithSubtext title="Digital tools" subtext="Designed to help nurture your development and growth." />
+                        <TitleWithSubtext title="Digital tools" subtext={featuresSubtext} />
                         <Container className={styles.featuresContainer} relative maxWidth >
                             {features.map((feature, index) => (
                                 <Container
@@ -319,31 +179,31 @@ const ReleaseScreen = ({ title, releaseDate, features, links, narrative, hasErro
                         <Container className={styles.narrativeDoodle2}></Container>
                         <Container className={styles.narrativeDoodle3}></Container>
                         <Container className={styles.narrativeDoodle4}></Container>
-                        <Text className={styles.narrativeText}>{narrative}</Text>
+                        <Text className={styles.narrativeText}>
+
+                        <b>where u goin shawty?</b>
+                        <br/>
+                        <br/>
+                        if u been on that daily hustle and grind but struggle to feel in tune with the journey or the meaning within.... then dude/dudette perk up your ears, cause the answer for you may be here...!
+                        <br/><br/>
+                        Dis space was designed to help connect with the transient, meaningful journey of personal development, to help u get ur yearning hands on <b>BALANCE</b> and <b>ABUNDANCE</b> with ease. 
+                        <br/>
+                        <br/>
+                        so if that don't sound like your cup of tea.... back off <b>SLIMEBALL</b> and stop reading NOW.
+                        <br/>
+                        <br/>
+                        Seeds grow and are nourished by water and stuff, and ideas are nourished too, and this seedling of an idea was inspired by sukkot, the jewish harvest festival, which explores themes of journeying, the transient human experience, and hereness... wow, woo, journeys, humans, hereness !? 
+                        <br/>
+                        <br/>
+                        and that's why this app gives transient spaces for journeying with life in a balanced way which invites YOU to live meaningfully and in a way which is authentic to yourself (...who else, silly!?) 
+                        <br/>
+                        <br/>
+                        Anyhoot, it's certainly incomplete - but that's cool, cause so are u!
+
+                        </Text>
                     </Container>
                 </Card>
                 <Card className={cx(styles.section, styles.linksSection)}>
-                    <Container
-                        className={styles.linksSpeciesBackground}
-                        style={{ backgroundImage: `url(${SpeciesImage})` }}
-                    />
-                    <Container
-                        className={styles.linksSukkahDwellingsBackground}
-                        style={{ backgroundImage: `url(${SukkahDwellingsImage})` }}
-                    />
-                    {/* <Container className={styles.bgEmojiContainer}>
-                        {emojis.map((emoji, index) => (
-                            <span
-                                key={index}
-                                className={styles.emoji}
-                                style={{
-                                    top: emoji.top,
-                                    left: emoji.left,
-                                    animationDuration: emoji.animationDuration
-                                }}
-                            >{emoji.emoji}</span>
-                        ))}
-                    </Container> */}
                     <Container className={styles.linksContainer}>
 
                         <TitleWithSubtext
@@ -377,7 +237,7 @@ const ReleaseScreen = ({ title, releaseDate, features, links, narrative, hasErro
                             <Container className={styles.secondaryButtons}>
                                 <Button
                                     className={cx(styles.funkyButton, styles.upcomingButton)}
-                                    text="What's next!"
+                                    text="What's new?!"
                                     onClick={() => setIsUpcomingDrawerOpen(true)}
                                 />
                                 <Button
@@ -397,7 +257,6 @@ const ReleaseScreen = ({ title, releaseDate, features, links, narrative, hasErro
 
                 <UpcomingModal isOpen={isUpcomingDrawerOpen} onClose={() => setIsUpcomingDrawerOpen(false)} />
                 <DrawerDialogue
-
                     isOpen={!!activeFeature}
                     onClose={() => setActiveFeature(null)}
                     className={styles.featureDrawer}
@@ -411,7 +270,7 @@ const ReleaseScreen = ({ title, releaseDate, features, links, narrative, hasErro
                     title="Booths Memberships!"
                     children={(
                         <Text>Members can access multiple booths, and more (upcoming) features, whilst subscription fees can help support this project to achieve sustainable growth.</Text>
-                )}
+                    )}
                 />
             </Container>
         </Screen>
